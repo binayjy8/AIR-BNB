a16260125c7b704552d134ec8f5453b6084fa92b
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-08 21:20:55 +0530

    backend database

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+async function main() {
+    
+}
+
 app.get("/", (req, res) => {
     res.send("hy, i'm the root");
 });
