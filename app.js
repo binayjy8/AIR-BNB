666297119a76ab93d3072ca496b423eb43d1bef4
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-09 20:27:29 +0530

    backend database

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const MONGO_URL = "mongodb://127.0.0.1:27017/test";
 
 async function main() {
-    await mongoose.connect()
+    await mongoose.connect();
 }
 
 app.get("/", (req, res) => {
