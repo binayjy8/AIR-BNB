0fcc27852b5de05464a486ea03ff2b8ef9931a29
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-15 10:16:07 +0530

    backend code

--- init/index.js
+++ init/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
-const Listing = require("../models/listings.js");
+const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -20,6 +20,6 @@ const initDB = async () => {
     await Listing.deleteMany({});
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
-}
+};
 
 initDB();
