97f2fa84f0d7ba03db28ab53856df397932680b9
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-17 20:11:56 +0530

    backend code

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.send("Hy, i'm the root");
