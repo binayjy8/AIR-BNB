cdf80ac94f4e0e38643af32a2cd1553b845407e7
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-20 15:00:21 +0530

    backend code

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.post("/listings", async (req, res) => {
 app.get("/listings/:id/edit", async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
 });
 
 // app.get("/testlisting", async (req, res) => {
