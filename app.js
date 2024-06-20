d05ad1a33b83168cebeb0c7af6a8aba608e5344a
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-20 15:45:56 +0530

    backend code

--- app.js
+++ app.js
@@ -59,6 +59,13 @@ app.put("/listings/:id", async (req, res) => {
     res.redirect(`/listings/${id}`);
 });
 
+app.delete("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+})
+
 // app.get("/testlisting", async (req, res) => {
 //     let sampleListing = new Listing({
 //         title: "My new Villa",
