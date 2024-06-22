91a2aeae11aa02167d6a9fb4582c816668351637
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-22 20:05:32 +0530

    backend code

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
