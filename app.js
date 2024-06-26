72da85d112c38a59747606cd89bfb220c314286a
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-26 20:14:56 +0530

    backend

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
     let {statusCode = 500, message = "Something Went Wong!"} = err;
-    res.render("error.ejs" , { err });
+    res.status(statusCode).render("error.ejs" , { message });
     // res.status(statusCode).send(message);
 });
 
