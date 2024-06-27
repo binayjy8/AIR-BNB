7821f689533bff92c795bda8d0021a555760f4ca
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-27 11:13:30 +0530

    backend

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -33,6 +34,16 @@ app.get("/", (req, res) => {
     res.send("Hi i am root");
 });
 
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body); 
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
 app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
@@ -48,11 +59,13 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
     res.render("listings/show.ejs", { listing });
 }));
 
-app.post("/listings",
+app.post("/listings", validateListing,
      wrapAsync(async (req, res, next) => {
-     if (!req.body.listing)  {
-        throw new ExpressError(400, "Send valid Data!");
-     }  
+        let result = listingSchema.validate(req.body); 
+        console.log(result);
+        if(result.error) {
+            throw new ExpressError(400, result.error);
+        }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -65,10 +78,8 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     res.render("listings/edit.ejs", { listing });
 }));
 
-app.put("/listings/:id", wrapAsync(async (req, res) => {
-    if (!req.body.listing)  {
-        throw new ExpressError(400, "Send valid Data!");
-     }  
+app.put("/listings/:id", validateListing, 
+    wrapAsync(async (req, res) => {  
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
