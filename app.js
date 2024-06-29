45d63a24040f97242fecec0c28a89351a26d2efc
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-29 18:26:41 +0530

    backend code

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -37,6 +37,16 @@ app.get("/", (req, res) => {
 
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body); 
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body); 
     if(error) {
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, errMsg);
@@ -93,7 +103,9 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
     res.redirect("/listings");
 }));
 
-app.post("/listings/:id/reviews", async (req, res) => {
+app.post("/listings/:id/reviews",
+    validateReview,
+    wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
 
@@ -106,7 +118,7 @@ app.post("/listings/:id/reviews", async (req, res) => {
     res.send("new review saved");
 
     res.redirect(`/listings/${listing._id}`);
-});
+}));
 
 app.all("*", (req, res, next) => {
     next(new ExpressError (404, "Page Not Found!"));
