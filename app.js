e188230a796ca12a3ffcc2df3331f1f168b92235
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-06-30 20:44:50 +0530

    backend code

--- app.js
+++ app.js
@@ -122,6 +122,12 @@ app.delete(
     "/listings/:id/reviews/:reviewId", 
     wrapAsync(async(req, res) => {
         let { id, reviewId } = req.params;
+
+
+        await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+        await Review.findByIdAndDelete(reviewId);
+
+        res.redirect(`/litings/${id}`);
     }));
 
 app.all("*", (req, res, next) => {
