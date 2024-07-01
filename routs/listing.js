4b548ed6c916b90402d9dfb89ec0e5f8d3fd9d0a
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-01 20:09:58 +0530

    file route

--- routs/listing.js
+++ routs/listing.js
@@ -15,3 +15,37 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
     const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", { listing });
 }));
+
+
+app.post("/listings", validateListing,
+    wrapAsync(async (req, res, next) => {
+       let result = listingSchema.validate(req.body); 
+       console.log(result);
+       if(result.error) {
+           throw new ExpressError(400, result.error);
+       }
+   const newListing = new Listing(req.body.listing);
+   await newListing.save();
+   res.redirect("/listings");
+   })
+);
+
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
+   let {id} = req.params;
+   const listing = await Listing.findById(id);
+   res.render("listings/edit.ejs", { listing });
+}));
+
+app.put("/listings/:id", validateListing, 
+   wrapAsync(async (req, res) => {  
+   let {id} = req.params;
+   await Listing.findByIdAndUpdate(id, {...req.body.listing});
+   res.redirect(`/listings/${id}`);
+}));
+
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
+   let {id} = req.params;
+   let deletedListing = await Listing.findByIdAndDelete(id);
+   console.log(deletedListing);
+   res.redirect("/listings");
+}));
