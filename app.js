afad3b3e631afe4b5313f5299b3caad7ff682a06
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-01 20:07:29 +0530

    file route

--- app.js
+++ app.js
@@ -55,20 +55,7 @@ const validateReview = (req, res, next) => {
     }
 };
 
-app.get("/listings", wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
-}));
 
-app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
-});
-
-app.get("/listings/:id", wrapAsync(async (req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs", { listing });
-}));
 
 app.post("/listings", validateListing,
      wrapAsync(async (req, res, next) => {
--- routs/listing.js
+++ routs/listing.js
@@ -1,2 +1,17 @@
 const express = require("express");
 const router = express.Router();
+
+app.get("/listings", wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+}));
+
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+app.get("/listings/:id", wrapAsync(async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id).populate("reviews");
+    res.render("listings/show.ejs", { listing });
+}));
