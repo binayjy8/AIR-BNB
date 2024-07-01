c7d29bae3a96ba3166149f86f5cfb2b433853057
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-01 21:43:42 +0530

    file route

--- app.js
+++ app.js
@@ -37,15 +37,7 @@ app.get("/", (req, res) => {
     res.send("Hi i am root");
 });
 
-const validateListing = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body); 
-    if(error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-};
+
 
 app.get("/listings", listings);
 
--- routs/listing.js
+++ routs/listing.js
@@ -4,6 +4,16 @@ const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
 
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body); 
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
 router.get("/", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("/index.ejs", { allListings });
