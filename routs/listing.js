f1c16ef53ef76d7523dc72d0fa14ea9677621810
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-01 21:45:33 +0530

    file route

--- routs/listing.js
+++ routs/listing.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const wrapAsync = require("./utils/wrapAsync.js");
-const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("./schema.js");
-const Listing = require("./models/listing.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("../schema.js");
+const Listing = require("../models/listing.js");
 
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body); 
