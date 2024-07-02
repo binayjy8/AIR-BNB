6d5f86b7f75027b53f0ec68f1e8541ef7f0a72e8
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-02 08:39:03 +0530

    file route

--- routes/review.js
+++ routes/review.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 //post review route
 router.post("/",
