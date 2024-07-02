87a677b7301ffb933748e4062447320327335a96
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-02 08:34:01 +0530

    file route

--- routes/review.js
+++ routes/review.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 //post review route
-app.post("/listings/:id/reviews",
+router.post("/listings/:id/reviews",
     validateReview,
     wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
@@ -17,7 +17,7 @@ app.post("/listings/:id/reviews",
 }));
 
 //Delete review rout
-app.delete(
+router.delete(
     "/listings/:id/reviews/:reviewId", 
     wrapAsync(async(req, res) => {
         let { id, reviewId } = req.params;
