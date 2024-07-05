2f189bc6ac3c18047e41a899e42a6468bbea8534
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-05 19:08:21 +0530

    backend

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const sessionOption = {
     secret: "mysupersecretcode",
     resave: false,
+    saveUninitialized: true
 }
 
 app.get("/", (req, res) => {
