d27ae54667e6f8589c12fbe918e383ef0ea8c46b
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-08 22:19:05 +0530

    backend

--- routes/listing.js
+++ routes/listing.js
@@ -50,6 +50,10 @@ router.post("/", validateListing,
 router.get("/:id/edit", wrapAsync(async (req, res) => {
    let {id} = req.params;
    const listing = await Listing.findById(id);
+   if(!listing) {
+    req.flash("error", "Listing you requested for does not exist");
+    res.redirect("/listings");
+}
    res.render("listings/edit.ejs", { listing });
 }));
 
