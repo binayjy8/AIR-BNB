f743a9c7da460d35ceddf65b64a8de9f25260bc8
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-09 16:35:16 +0530

    backend

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.use(session(sessionOptions));
 app.use(flash());
 
 app.use(passport.initialize());
+app.use(passport.session());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
