f3f2cf86c89ffac2178f1dc033d76af870bdfbf6
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-10 21:20:38 +0530

    backend

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-app.get("/signup", (req, res) => {})
+app.get("/signup", (req, res) => {
+    res.send("form");
+});
 
 module.exports = router;
