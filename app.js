27beadbbe093f2a8b2fe99f83af72a6dee28db24
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-10 21:46:26 +0530

    backend

--- app.js
+++ app.js
@@ -80,6 +80,7 @@ app.get("/demouser", async (req, res) => {
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 
 app.all("*", (req, res, next) => {
