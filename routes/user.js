b4cbe5452df32acf8f56918ca73db5b8c938487a
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-13 11:47:21 +0530

    backend

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
@@ -33,7 +34,8 @@ router.get("/login", (req, res) => {
 });
 
 router.post(
-    "/login", 
+    "/login",
+    saveRedirectUrl, 
     passport.authenticate("local", { 
         failureRedirect: "/login", 
         failureFlash: true
