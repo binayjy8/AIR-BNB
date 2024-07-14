9e67740771f1d1b5ec5122be3c48a664d81da01a
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-14 21:26:12 +0530

    backend

--- middleware.js
+++ middleware.js
@@ -14,4 +14,11 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     next();
 };
 
-module.exports.isOwner = (req, res, next)
+module.exports.isOwner = async (req, res, next) => {
+    let {id} = req.params;
+   let listing = await Listing.findById(id);
+   if(!listing.owner.equals(res.locals.crrUser._id)) {
+    req.flash("error", "you dont't have permission to edit");
+    return res.redirect(`/listings/${id}`);
+   }
+}
