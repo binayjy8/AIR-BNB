2f725aaf17624762e4669d84bd1b7b21a8a1d900
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-14 08:20:01 +0530

    backend

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data.map((obj) => ({...obj, owner: "668f5cb4a8dd8c1a29c5e57f"}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 };
