cbfdd3a1e266ee1778b5d47899cfee16c6829173
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-15 10:31:00 +0530

    backend

--- middleware.js
+++ middleware.js
@@ -50,11 +50,11 @@ module.exports.validateReview = (req, res, next) => {
     }
 };
 
-module.exports.isOwner = async (req, res, next) => {
-    let {id} = req.params;
-   let listing = await Listing.findById(id);
-   if(!listing.owner.equals(res.locals.crrUser._id)) {
-    req.flash("error", "you dont't have permission to edit");
+module.exports.isReviewAuthor = async (req, res, next) => {
+    let {id, reviewId} = req.params;
+   let review = await Review.findById(reviewIdid);
+   if(!review.owner.equals(res.locals.crrUser._id)) {
+    req.flash("error", "you are not author");
     return res.redirect(`/listings/${id}`);
    }
 
