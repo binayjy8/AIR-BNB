65bf98ec7f0fd42dc333e30cad85f3fc65313d4b
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-15 08:25:02 +0530

    backend

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 const Listing = require("./models/listing");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
@@ -31,6 +31,16 @@ module.exports.isOwner = async (req, res, next) => {
 
 module.exports.validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body); 
+    if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
+module.exports.validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body); 
     if(error) {
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, errMsg);
