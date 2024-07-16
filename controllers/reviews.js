ea17f0d6a2ca4b97d15f610ef8abc455db500f13
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-16 10:20:50 +0530

    backend route

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -13,3 +13,14 @@ module.exports.createReview = (async (req, res) => {
     req.flash("success", "New Review Created!");
     res.redirect(`/listings/${listing._id}`);
 });
+
+module.exports.destroyReview = (async(req, res) => {
+    let { id, reviewId } = req.params;
+
+
+    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+
+    req.flash("success", "Review Deleted!");
+    res.redirect(`/listings/${id}`);
+});
--- routes/review.js
+++ routes/review.js
@@ -20,15 +20,6 @@ router.delete(
     "/:reviewId", 
     isLoggedIn,
     isReviewAuthor,
-    wrapAsync(async(req, res) => {
-        let { id, reviewId } = req.params;
-
-
-        await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-        await Review.findByIdAndDelete(reviewId);
-
-        req.flash("success", "Review Deleted!");
-        res.redirect(`/listings/${id}`);
-    }));
+    wrapAsync(reviewController.destroyReview));
 
     module.exports = router;
