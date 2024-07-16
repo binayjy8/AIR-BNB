5054e14ee7240cc8bd1473f65a60617ae6810a7f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-16 10:37:54 +0530

    backend route

--- controllers/users.js
+++ controllers/users.js
@@ -20,4 +20,24 @@ module.exports.signup = async(req, res)=> {
         res.redirect("/signup");
     }
     
-}
+};
+
+module.exports.renderLoginForm = (req, res) => {
+    res.render("users/login.ejs");
+};
+
+module.exports.login = async(req, res) => {
+    req.flash("success", "welcome to wanderlust!");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
+};
+
+module.exports.logout = (req, res, next) => {
+    req.logOut((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "you are logged out!");
+        res.redirect("/listings");
+    });
+};
--- routes/user.js
+++ routes/user.js
@@ -11,9 +11,7 @@ router.get("/signup", userController.renderSignupForm);
 
 router.post("/signup", wrapAsync(userController.signup));
 
-router.get("/login", (req, res) => {
-    res.render("users/login.ejs");
-});
+router.get("/login", userController.renderLoginForm);
 
 router.post(
     "/login",
@@ -22,20 +20,9 @@ router.post(
         failureRedirect: "/login", 
         failureFlash: true
     }), 
-    async(req, res) => {
-        req.flash("success", "welcome to wanderlust!");
-        let redirectUrl = res.locals.redirectUrl || "/listings";
-        res.redirect(redirectUrl);
-});
+    userController.login
+    );
 
-router.get("/logout", (req, res, next) => {
-    req.logOut((err) => {
-        if (err) {
-            return next(err);
-        }
-        req.flash("success", "you are logged out!");
-        res.redirect("/listings");
-    });
-});
+router.get("/logout", userController.logout);
 
 module.exports = router;
