920c210f475801cf514fe5f78035dd9be57e1a43
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-16 10:19:08 +0530

    backend route

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,3 +1,6 @@
+const Listing = require("../models/listing");
+const Review = require("../models/review");
+
 module.exports.createReview = (async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
