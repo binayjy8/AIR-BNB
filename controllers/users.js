b9d4b9ffd71ba53a5b51b139dd655cd66563c203
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-16 10:29:11 +0530

    backend route

---
+++ controllers/users.js
@@ -0,0 +1,19 @@
+module.exports.signup = async(req, res)=> {
+    try {
+        let {username, email, password} = req.body;
+    const newUser = new User({email, username});
+    const registerdUser = await User.register(newUser, password);
+    console.log(registerdUser);
+    req.login(registerdUser, (err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "Welcome to Wanderlust!");
+        res.redirect("/listings");
+    });
+    } catch(e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+    
+}
--- routes/user.js
+++ routes/user.js
@@ -9,25 +9,7 @@ router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
 
-router.post("/signup", wrapAsync(async(req, res)=> {
-    try {
-        let {username, email, password} = req.body;
-    const newUser = new User({email, username});
-    const registerdUser = await User.register(newUser, password);
-    console.log(registerdUser);
-    req.login(registerdUser, (err) => {
-        if (err) {
-            return next(err);
-        }
-        req.flash("success", "Welcome to Wanderlust!");
-        res.redirect("/listings");
-    });
-    } catch(e) {
-        req.flash("error", e.message);
-        res.redirect("/signup");
-    }
-    
-}));
+router.post("/signup", wrapAsync());
 
 router.get("/login", (req, res) => {
     res.render("users/login.ejs");
