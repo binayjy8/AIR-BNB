04fe43063c3076eca1d3fafafffed508f5da00d8
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-16 10:16:41 +0530

    backend route

--- routes/review.js
+++ routes/review.js
@@ -5,6 +5,7 @@ const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
+const reviewController = require("../controllers/reviews.js");
 
 
 
@@ -12,18 +13,7 @@ const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js
 router.post("/",
     isLoggedIn,
     validateReview,
-    wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    req.flash("success", "New Review Created!");
-    res.redirect(`/listings/${listing._id}`);
-}));
+    wrapAsync);
 
 //Delete review rout
 router.delete(
