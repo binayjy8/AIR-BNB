f349e9284a78f093f4f3f3cd483602ea11f4b57f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-07-17 13:26:17 +0530

    backend route

--- routes/listing.js
+++ routes/listing.js
@@ -7,6 +7,7 @@ const { isLoggedIn, isOwner,validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 
 const multer  = require("multer");
+const { storage } = require("../cloudConfig.js");
 const upload = multer({ dest: "uploads/" });
 
 router
